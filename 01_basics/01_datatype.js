23d81bb3adab7c2f8df5388443c7a82617dd9ee7
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-16 14:07:09 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //use js code new version
+
+// alert(3+3)  //we use node js not browser
+
+let ava=639340
+let ct="avan"
+let isLen=false
+// number=> 2 to power 53
+//string =>" "
+//boolean =>true/false
+//Null =>standlone value
+//undefined=>
+//symbol=>unique
+
+//object
+//console.log(typeof "avaneesh")
+console.log(typeof null);
