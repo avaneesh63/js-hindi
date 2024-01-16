e3ccebf24472d6cfe0fd7f34b35e00fb1b308c1c
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-16 14:05:56 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+const acountId=786
+let acountEmail="avnish@gmail.com"
+var acountPass="639340"
+acountCity="delhi"
+acountEmail="avsh@gmail.com"
+acountPass="6393406378"
+acountCity="noida"
+let aci;
+/*
+modern day var are not use
+var are global scope variable
+let are block scope variable
+*/
+console.table([acountEmail,acountPass,acountCity,aci])
+
+//acountId=567   //not allowed
+console.log(acountId);
