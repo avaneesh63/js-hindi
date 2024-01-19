e1cfb83e3926f707cf754313582ee787e541e4d9
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-19 19:58:31 +0000

    operation

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,11 +1,37 @@
-let score="33"
+// let score="33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber=Number(score)
+// let valueInNumber=Number(score)
 
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
 //NaN=> "123abc"
 //true=> 1    false=>0
 //"123"=>123
+
+
+
+//*********Operation******** 
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2)
+
+// let str1="avaneesh"
+// let str2=" kumar"
+// let str3=str1+str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("2"+1);
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+// console.log(true)
+// console.log(+true);
+// console.log(+"");
