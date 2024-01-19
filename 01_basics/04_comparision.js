7431345b4d16beea2658269d94e650373d1082a9
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-19 20:12:52 +0000

    comapre

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+// c
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>=0);
+
+// console.log(2==1);
+// console.log("2"===1); //not convert
+// === is compare value and datatype
