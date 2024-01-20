cc0f087a73e107fc89614b2ab43a6535a93b622b
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-20 09:06:23 +0000

    datatype interview

---
+++ 01_basics/datatype.js
@@ -0,0 +1,54 @@
+// // primitive
+// Call by value
+
+// // 7 type:  
+// // Number
+//  const value=45
+//  console.log(typeof value);
+// // String
+// const Svalue="345"
+// console.log(typeof Svalue);
+// // Boolean
+//  const Bvalue=true
+//  console.log(Bvalue);
+// // undefind
+// let Numbersy;
+// console.log(typeof Numbersy);
+// // Symbol
+// const sym=Symbol('345')
+// let syms=Symbol('345')
+// console.log(typeof sym);
+
+// // null
+// const nullValue=null
+// console.log(typeof nullValue);
+
+// // BigInt
+// const bigNumber=2345678
+// console.log(typeof bigNumber);
+
+
+
+
+
+// // reference or non primitive
+//call by reference
+
+// // Object
+// const Myobj={
+//     age:45,
+//     name:"avaneesh",
+
+
+// }
+// console.log(typeof Myobj);
+// // Array
+// const myarray=["ram","dhyam","shaktiman"]
+// console.log(typeof myarray);
+// // functions
+// const myFuction=function(){
+//     console.log("hello world");
+// }
+// console.log(myFuction);
+
+
