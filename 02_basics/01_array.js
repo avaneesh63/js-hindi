d5979b1f0e0a49bffefbfc93bb6305e17cc60cd0
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-24 19:41:08 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+// const myArra=[0,1,2,3,4,5,6]
+// console.log(myArra);
+
+// const myArra2=new Array(2,3,4,4,5,6,7)
+// console.log(myArra2);
+// const myArr1=["shaktiman","ram","ravan"]
+// console.log(myArr1);
+
+// ///array method
+
+// myArra.push(4)
+// console.log(myArra);
+// myArra.pop(4)
+// console.log(myArra);
+
+
+ const myArr1=[2,3,4,5,6]
+ myArr1.unshift(9)
+ console.log(myArr1);
+
+ //slice include the rannge without last
+ //splice  include the range and manipulate the array
