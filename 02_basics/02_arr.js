4456eca64a9af6b4bdad7fc72c32e0fd0bed7718
Avaneesh Kumar <87476604+avaneesh63@users.noreply.github.com>
2024-01-24 20:21:31 +0000

    array2

---
+++ 02_basics/02_arr.js
@@ -0,0 +1,14 @@
+// const ind=["ytre","ram","shyam"]
+// const indi=["putra","pita","bhai"]
+// ind.push(indi)
+// console.log(ind);
+
+// const newind=ind.concat(indi)
+// console.log(newind);
+// const india=[...ind,...indi]
+// console.log(india);
+
+let score1=100
+let score2=200
+let score3=300
+console.log(Array.of(score1,score2,score3));
